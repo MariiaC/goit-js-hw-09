e848389d260e7a8184c76ff26c9d2fc08ab98810
MariiaC <mariiachvorun@gmail.com>
2022-08-12 21:11:04 +0300

    upd-2-3-tasks-js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,7 +3,73 @@ import flatpickr from "flatpickr";
 // Додатковий імпорт стилів
 import "flatpickr/dist/flatpickr.min.css";
 import Notiflix from 'notiflix';
-//дано в умові розрах 
+
+const refs = {
+  // inputPicker: document.querySelector('#datetime-picker'),
+  startBtn: document.querySelector('[data-start]'),
+  spanDays: document.querySelector('[data-days]'),
+  spanHours: document.querySelector('[data-hours]'),
+  spanMinutes: document.querySelector('[data-minutes]'),
+  spanSeconds: document.querySelector('[data-seconds]'),
+};
+refs.startBtn.disabled = true;
+refs.startBtn.addEventListener('click', startClick);
+let countDownTimer = null;
+
+
+
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+        if (Date.parse(selectedDates) < Date.parse(new Date())) {
+            Notiflix.Notify.failure('❌ Please choose a date in the future')
+        } 
+        countDownTimer = Date.parse(selectedDates[0]) - Date.parse(new Date());
+        refs.startBtn.disabled=false
+    }
+}
+
+
+function startClick() {
+    let timer = setInterval(() =>{
+const { days, hours, minutes, seconds } = convertMs(
+      (countDownTimer = countDownTimer - 1000)
+    );
+    const dd = days.toString();
+    const hh = hours.toString();
+    const mm = minutes.toString();
+        const ss = seconds.toString();
+
+    refs.spanDays.textContent =
+      dd.length > 1 ? dd : addLeadingZero(dd);
+    refs.spanHours.textContent =
+      hh.length > 1 ? hh : addLeadingZero(hh);
+    refs.spanMinutes.textContent =
+      mm.length > 1 ? mm : addLeadingZero(mm);
+    refs.spanSeconds.textContent =
+      ss.length > 1 ? ss : addLeadingZero(ss);
+  }, 1000);
+}
+
+flatpickr('#datetime-picker', options);
+
+
+
+
+//додаємо 0 поперед цифри для фен-шуя по умові)
+function addLeadingZero(value) {
+  return value.padStart(2,'0')
+};
+// function setPad(days = '0', hours = '0', minutes = '0', seconds = '0') {
+//   refs.spanDays.textContent = addLeadingZero(String(days));
+//   refs.spanHours.textContent = addLeadingZero(String(hours));
+//   refs.spanMinutes.textContent = addLeadingZero(String(minutes));
+//   refs.spanSeconds.textContent = addLeadingZero(String(seconds));
+// }
+//за умовою
 function convertMs(ms) {
   // Number of milliseconds per unit of time
   const second = 1000;
@@ -12,7 +78,7 @@ function convertMs(ms) {
   const day = hour * 24;
 
   // Remaining days
-  const days = Math.floor(ms / day);
+  const days = (Math.floor(ms / day));
   // Remaining hours
   const hours = Math.floor((ms % day) / hour);
   // Remaining minutes
@@ -21,52 +87,4 @@ function convertMs(ms) {
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
-}
-
-
-// const refs = {
-//     input: document.querySelector('#datetime-picker'),
-//     startBtn: document.querySelector("[data-start]"),
-//     spanDays: document.querySelector("[data-days]"),
-//     spanHours: document.querySelector("[data-hours]"),
-//     spanMinutes: document.querySelector("[data-minutes]"),
-//     spanSeconds: document.querySelector("[data-seconds]"),
-// }
-// //console.log(refs);
-
-// refs.startBtn.addEventListener('click', startClick);
-// refs.startBtn.disabled = true;
-
-// function startClick(evt) {
-//   if (timer2) {
-//     clearTimeout(timer2);
-//     timer2 = null;
-//   }
-//   timer = setInterval(updateTimerDisplay, 1000);
-//   btnStartRef.disabled = true;
-// }
-
-// function pad(value) {
-//     return String(value).padStart(2,'0')
-// };
-
-// function convertMs(ms) {
-//   // Number of milliseconds per unit of time
-//   const second = 1000;
-//   const minute = second * 60;
-//   const hour = minute * 60;
-//   const day = hour * 24;
-
-//   // Remaining days
-//   const days = pad(Math.floor(ms / day));
-//   // Remaining hours
-//   const hours = pad(Math.floor((ms % day) / hour));
-//   // Remaining minutes
-//   const minutes = pad(Math.floor(((ms % day) % hour) / minute));
-//   // Remaining seconds
-//   const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
-
-//   return { days, hours, minutes, seconds };
-// };
-
-// // console.log(`${days}:${hours}:${minutes}:${seconds}`);
+  };
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,42 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+
+import Notiflix from 'notiflix';
+
+const refs = {
+  formClass: document.querySelector('.form'),
+  delayNumber: document.querySelector('[name="delay"]'),
+  stepNumber: document.querySelector('[name="step"]'),
+  amountNumber: document.querySelector('[name="amount"]'),
+};
+
+refs.formClass.addEventListener('submit', promiseClick);
+
+function promiseClick(event) {
+  event.preventDefault();
+
+  const firstDelay = Number(refs.delayNumber.value);
+  const delayStep = Number(refs.stepNumber.value);
+
+  for (let i = 0; i < refs.amountNumber.value; i+=1) {
+    createPromise(i + 1, firstDelay + delayStep * i)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
+};
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      }// Fulfill
+      reject({ position, delay });
+    }, delay); // Reject
+  })
+  
 }
